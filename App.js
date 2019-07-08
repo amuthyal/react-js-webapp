04e5f70473c6c2252d5bc6471e4926c67f41f9cb
Akhila Muthyala <31458831+amuthyal@users.noreply.github.com>
2019-07-07 23:49:54 -0700

    Update App.js
    Dynamically update lists (persons) by using map() method. we are making the javascript code to look like jsx so that the DOM will render it

--- App.js
+++ App.js
@@ -53,22 +53,11 @@ class App extends Component {
     if(this.state.showPersons){
         persons= (
           <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            changed= {this.nameChangedHandler}
-            click={this.switchNameHandler.bind(this,"Max!")}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map(person => {
+              return <Person name={person.name} age={person.age}/>
+            
+               } )
+          }
           </div> 
         );
     }
